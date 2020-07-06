0cbc91314d25386c540b6f4b71224e762fc75784
DimaFichkovskiy <fichkovskiy123@gmail.com>
2020-07-06 16:18:55 +0300

    Created a database.py file

--- database.py
+++ database.py
@@ -2,14 +2,28 @@ import sqlalchemy as db
 from sqlalchemy.ext.declarative import declarative_base
 from config import Config
 
-engine = ''
+engine = Config.DB_URI
 Base = declarative_base(bind=engine)
 
 
-class Works(Base):
-    __tablename__ = 'works'
+class freelancehunt_data(Base):
+    __tablename__ = 'freelancehunt_data'
 
     id = db.Column(db.Integer(), primary_key=True)
     date = db.Column(db.String(20))
-    title = db.Column(db.String(120), unique=True)
-    url = db.Column(db.String(200), unique=True)
+    title = db.Column(db.String(120))
+    url = db.Column(db.String(200))
+
+
+class DBDriver:
+    @classmethod
+    def add_work(cls, works):
+        with engine.connect() as connection:
+            if not any(works):
+                return None
+            insert_query = db.insert(freelancehunt_data)
+            connection.execute(insert_query, works)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all()
