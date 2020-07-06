00c2405bf6b436185c40598899ae8709eaf8a3bc
DimaFichkovskiy <fichkovskiy123@gmail.com>
2020-07-06 16:12:46 +0300

    Created a config.py file

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
+import psycopg2
+from sqlalchemy import create_engine
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    pass
+    user = os.environ['username']
+    db_name = os.environ['db_name']
+    port = os.environ['port']
+    password = os.environ['password']
+    host = os.environ['host']
+
+    DB_URI = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}')
